f5d9db51315fde0d15177833f936f5dbde3c6aef
sanchico <sanchico2000-git@yahoo.es>
2015-08-03 01:05:08 +0200

    Modelo y base de datos
    probado con estas versiones: node 0.12.4 sequelize 1.7.0 y sqlite3 3.0.9

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,14 +2,14 @@ var models = require('../models/models.js');
 
 // GET /quizes/question
 exports.question = function(req,res) {
-	models.Quiz.findall().success(function(quiz) {
+	models.Quiz.findAll().success(function(quiz) {
 		res.render('quizes/question', {pregunta: quiz[0].pregunta})
 	})
 };
 //Quiz.findall devuelve el contenido de la base de datos en forma de array
 // GET /quizes/answer
 exports.answer = function(req,res) {
-	models.Quiz.findall().success(function(quiz) {
+	models.Quiz.findAll().success(function(quiz) {
 		if(req.query.respuesta === quiz[0].respuesta) {
 			res.render('quizes/answer', {respuesta: 'Correcto'});
 		} else {
