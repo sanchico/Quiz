b37d36cd2dea1852bb658911d80213c598407e86
sanchico <sanchico2000-git@yahoo.es>
2015-08-04 02:22:51 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ var quizController = require ('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Definición de rutas de /quizes. \\d+ = uno o mas numeros decimales
+router.get('/quizes',                          quizController.index);
+router.get('/quizes/:quizId(\\d+)',           quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',    quizController.answer);
+
 router.get('/author', quizController.author);
 
 module.exports = router;
