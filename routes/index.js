8418f108b534dd5fa8d9da80cb5021012adea8ba
sanchico <sanchico2000-git@yahoo.es>
2015-08-09 18:41:33 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ var quizController = require ('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
 // Definición de rutas de /quizes. \\d+ = uno o mas numeros decimales
 router.get('/quizes',                          quizController.index);
 router.get('/quizes/:quizId(\\d+)',           quizController.show);
