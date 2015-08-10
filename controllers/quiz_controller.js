9f21c8043bb30ed179ef846ebd5051eab7116f28
sanchico <sanchico2000-git@yahoo.es>
2015-08-10 03:00:22 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -7,22 +7,22 @@ exports.load = function(req, res, next, quizId) {
 			if (quiz) {
 				req.quiz = quiz;
 				next();
-			} else { next ( new errror('No existe quizId=' + quizId)); }
+			} else { next (new Error('No existe quizId=' + quizId)); }
 		}
 	).catch(function(error){ next(error);});
-}
+};
 // GET /quizes
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(
 		function(quizes) {
-			res.render('quizes/index.ejs', { quizes: quizes});
+			res.render('quizes/index', { quizes: quizes});
 		}
 	).catch(function(error) { next(error);})
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-	res.render('quizes/show', {quiz: quiz});
+	res.render('quizes/show', {quiz: req.quiz});
 };
 
 // GET /quizes/:id/answer
@@ -31,9 +31,24 @@ exports.answer = function(req,res) {
 	if(req.query.respuesta === req.quiz.respuesta) {
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer',{ quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
 };
 
 exports.author = function(req,res) {
 	res.render('author');
 }
+//GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( //metodo heredado de sequelize
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz});
+};
+//POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	}) //redireccion HTTP ¿302?
+};
--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,11 @@ router.get('/', function(req, res) {
 router.param('quizId', quizController.load);
 
 // Definición de rutas de /quizes. \\d+ = uno o mas numeros decimales
-router.get('/quizes',                          quizController.index);
+router.get('/quizes',                         quizController.index);
 router.get('/quizes/:quizId(\\d+)',           quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',    quizController.answer);
-
+router.get('/quizes/new',                     quizController.new);
+router.post('/quizes/create',                 quizController.create);
 router.get('/author', quizController.author);
 
 module.exports = router;
