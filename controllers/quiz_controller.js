803f3aad7c868285a84af5fddbeb80acd32d8642
sanchico <sanchico2000-git@yahoo.es>
2015-08-12 12:36:23 +0200

    Validación
    Al provocar un fallo de validación aparece el siguiente error:
    undefined is not a function
    undefined
    
    TypeError: undefined is not a function
    at exports.create
    (D:\MiriadaX\mis_proyectos\quiz\controllers\quiz_controller.js:54:3)

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -9,20 +9,20 @@ exports.load = function(req, res, next, quizId) {
 				next();
 			} else { next (new Error('No existe quizId=' + quizId)); }
 		}
-	).catch(function(error){ next(error);});
+	).catch(function(error){ next(error)});
 };
 // GET /quizes
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(
 		function(quizes) {
-			res.render('quizes/index', { quizes: quizes});
+			res.render('quizes/index.ejs', { quizes: quizes, errors: []});
 		}
-	).catch(function(error) { next(error);})
+	).catch(function(error) { next(error)});
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -31,24 +31,36 @@ exports.answer = function(req,res) {
 	if(req.query.respuesta === req.quiz.respuesta) {
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', { quiz: req.quiz,
+																respuesta: resultado,
+																errors: []});
 };
 
 exports.author = function(req,res) {
-	res.render('author');
+	res.render('author', { errors: []});
 }
 //GET /quizes/new
 exports.new = function(req, res) {
-	var quiz = models.Quiz.build( //metodo heredado de sequelize
+	var quiz = models.Quiz.build( //metodo heredado de sequelize. creea pbjeto quiz
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 	);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 //POST /quizes/create
 exports.create = function(req, res) {
 	var quiz = models.Quiz.build( req.body.quiz );
-
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	}) //redireccion HTTP ¿302?
+	quiz
+	.validate()
+	.then(
+		function(err) {
+			if(err) {
+				res.render('quizes.new', {quiz: quiz, errors: err.errors});
+			} else {
+				quiz
+				.save({fields: ["pregunta", "respuesta"]})
+				.then( function(){
+					res.redirect('/quizes')}) //redireccion HTTP ¿302?
+			}
+		}
+	);
 };
--- models/quiz.js
+++ models/quiz.js
@@ -1,7 +1,15 @@
 //Definicion del modelo de quiz
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Quiz',
-            { pregunta: DataTypes.STRING,
-              respuesta: DataTypes.STRING,
-            });
+  return sequelize.define(
+    'Quiz',{
+      pregunta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "--> Pregunta no puede estar vacía"}}
+      },
+      respuesta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "--> Falta Respuesta"}}
+      }
+    }
+  );
 }
