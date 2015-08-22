8913d0759adaa3079e831e6d9f0e3cbc22fbf9d6
sanchico <sanchico2000-git@yahoo.es>
2015-08-22 12:49:50 +0200

    moderación comentarios

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ router.get('/', function(req, res) {
 });
 
 //Autoload de comandos con :quizId
-router.param('quizId', quizController.load);
+router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', commentController.load); //autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -30,7 +31,8 @@ router.delete('/quizes/:quizId(\\d+)',        sessionController.loginRequired, q
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
-
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+            sessionController.loginRequired, commentController.publish);
 router.get('/author', quizController.author);
 
 module.exports = router;
