5315da2e0393b6a477e7b637ceca84a132d7c4fa
sanchico <sanchico2000-git@yahoo.es>
2015-08-23 17:24:58 +0200

    HTTPS certificados
    No añadí los certificados a github, por eso fallaba en heroku

--- comandos-para-generar-claves.ps1
+++ comandos-para-generar-claves.ps1
@@ -6,4 +6,8 @@ openssl genrsa -out quiz-2015-key.pem 2048
 
 openssl req -new -sha256 -key quiz-2015-key.pem -out quiz-2015-csr.pem
 
+<<<<<<< HEAD
 openssl x509 -req -in quiz-2015-csr.pem -signkey quiz-2015-key.pem -out quiz-2015-cert.pem
+=======
+openssl x509 -req -in quiz2015-csr.pem -signkey quiz-2015.pem -out quiz-2015-cert.pem
+>>>>>>> origin/master
